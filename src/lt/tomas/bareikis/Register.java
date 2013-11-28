dfb15647356a31a2164f3c94531c440b5998f2e3
Tomas <tomuibar@gmail.com>
2013-11-28 22:49:04 +0200

    Added Register class

---
+++ src/lt/tomas/bareikis/Register.java
@@ -0,0 +1,48 @@
+package lt.tomas.bareikis;
+
+public class Register {
+
+    Bit value;
+
+    public Register() {
+        this.value = new Bit(2, 0);
+    }
+
+    public Register(Bit value) {
+        this.value = value;
+    }
+
+    public Bit getValue() {
+        return value;
+    }
+
+    public int getValueAsInt() {
+        return value.getValue();
+    }
+
+    public String getValueAsString() {
+        return String.valueOf(this.getValueAsInt());
+    }
+
+    public void setValue(Bit value) throws IllegalArgumentException{
+        this.value = value;
+    }
+
+    public void setValue(int valueToSet) {
+        this.value.setValue(valueToSet);
+    }
+
+    public void addValue(Bit valueToAdd) throws IllegalArgumentException{
+        this.value.add(valueToAdd);
+    }
+
+    public void addValue(int valueToAdd) {
+        this.value.add(valueToAdd);
+    }
+
+    public void nullifyValue() {
+        this.value.nullify();
+    }
+
+
+}
