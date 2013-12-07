b25fc5a3d8165af3dfd1f6cbf8d716bd442da46a
Tomas <tomuibar@gmail.com>
2013-12-07 23:53:45 +0200

    Added image sending window elements. Without any actions yet.

--- src/lt/tomas/bareikis/GUI/ImageWindow.java
+++ src/lt/tomas/bareikis/GUI/ImageWindow.java
@@ -1,17 +1,30 @@
 package lt.tomas.bareikis.GUI;
 
 
+import lt.tomas.bareikis.GUI.customComponents.ProbabilityJTextField;
+import lt.tomas.bareikis.GUI.customComponents.StringInputJTextArea;
+import lt.tomas.bareikis.GUI.customComponents.VectorInputJTextArea;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class ImageWindow extends JFrame {
 
     private JPanel rootJPanel;
+    private JButton chooseImageButton;
+    private JButton encodeButton;
+    private VectorInputJTextArea encodedTextArea;
+    private VectorInputJTextArea inputImageTextArea;
+    private ProbabilityJTextField errorProbabilityTextField;
+    private JButton sendChannelButton;
+    private VectorInputJTextArea transferedVectorTextArea;
+    private StringInputJTextArea transferedStringTextArea;
+    private JButton decodeButton;
 
     public ImageWindow() {
         super("Paveikslėlio siuntimas");
 
-        rootJPanel.setPreferredSize(new Dimension(600, 600));
+        rootJPanel.setPreferredSize(new Dimension(700, 600));
 
         this.setContentPane(rootJPanel);
         pack();
