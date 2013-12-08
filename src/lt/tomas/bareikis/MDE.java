35289a26d2c15832963cecf12f29109375b44478
Tomas <tomuibar@gmail.com>
2013-12-08 21:27:25 +0200

    Added comments for MDE class

--- src/lt/tomas/bareikis/MDE.java
+++ src/lt/tomas/bareikis/MDE.java
@@ -3,14 +3,25 @@ package lt.tomas.bareikis;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Nustato, kurios iš paduotų reikšmių sekoje yra daugiausia.
+ */
 public class MDE {
 
     public MDE() {}
 
+    /**
+     * Nustato, kurios iš paduotų reikšmių sekoje yra daugiausia.
+     * Jei pasikartoja kelios reikšmės tiek pat kartų, tai imama pirma sąraše.
+     *
+     * @param dataStream duomenų seka
+     * @return daugiausiai pasikartojimų turinti reikšmė
+     */
     public Bit calculate(DataStream dataStream) {
 
-        Bit result = new Bit();
+        Bit result = new Bit(); // daugiausiai kartų pasikartojusi reikšmė
 
+        // Sukuriame rakto - reikšmės porų sąrašą. Raktas - bito reikšmė, veikšmė - pasikartojimų skaičius
         Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
 
         for (Bit bit: dataStream.getDataAsBitList()) {
@@ -19,10 +30,14 @@ public class MDE {
 
         int maxOccurrences = 0;
 
+        // Randame daugiausiai kartų pasikartojančią reikšmę
         for (Map.Entry<Integer, Integer> valuePairs : frequencyMap.entrySet()) {
-            Integer numberOfOccurrences = valuePairs.getValue();
-            Integer bitValue = valuePairs.getKey();
+
+            Integer numberOfOccurrences = valuePairs.getValue(); // Pasikartojimų skaičius
+            Integer bitValue = valuePairs.getKey(); // Bito reikšmė
+
             if (numberOfOccurrences > maxOccurrences) {
+                // Radome daugiau kartų pasikartojusią reikšmę
                 result = new Bit(bitValue);
                 maxOccurrences = numberOfOccurrences;
             }
@@ -31,12 +46,19 @@ public class MDE {
         return result;
     }
 
+    /**
+     * Prideda naują reikšmę rakto - reikšmės porų sąraše
+     *
+     * @param frequencyMap rakto - reikšmės porų sąrašas
+     * @param bit nauja reikšmė
+     */
     private void storeValue(Map<Integer, Integer> frequencyMap, Bit bit) {
         if (frequencyMap.get(bit.getValue()) != null) {
+            // Jei jau tokia reikšmė yra, tai jos pasikartojimų skaičių padidiname vienetu
             frequencyMap.put(bit.getValue(), frequencyMap.get(bit.getValue())+1);
         } else {
+            // Jei ne, tai pridedame tą reikšmė, su pasikartojimų skaičiumi, lygiu 1
             frequencyMap.put(bit.getValue(), 1);
         }
     }
-
 }
