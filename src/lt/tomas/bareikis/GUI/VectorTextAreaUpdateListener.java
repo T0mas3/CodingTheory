9d7bdfba7c057d911d12855c48c6e5a0ed705421
Tomas <tomuibar@gmail.com>
2013-12-10 00:37:34 +0200

    Comments and refactoring for Vector Text Area Update Listener

--- src/lt/tomas/bareikis/GUI/VectorTextAreaUpdateListener.java
+++ src/lt/tomas/bareikis/GUI/VectorTextAreaUpdateListener.java
@@ -6,6 +6,9 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 
+/**
+ * Kas kartą pasikeitus tekstui vektoriaus lauke, jis yra validuojamas
+ */
 public class VectorTextAreaUpdateListener implements DocumentListener {
 
     JTextArea jTextArea;
@@ -30,11 +33,24 @@ public class VectorTextAreaUpdateListener implements DocumentListener {
         this.validateInput();
     }
 
+    /**
+     * Jei vektorius nėra validus (sudarytas ne tik iš 0 ir 1), tai tekstas nuspalvinamas kita spalva
+     */
     private void validateInput() {
-        if (Helper.isVectorInputValid(jTextArea.getText())) {
+        if (this.isVectorInputValid(jTextArea.getText())) {
             jTextArea.setForeground(Color.black);
         } else {
             jTextArea.setForeground(Color.magenta);
         }
+    }
+
+    /**
+     * Ar vektorius yra tinkamas (sudarytas tik iš 0 ir 1s)
+     *
+     * @param input tekstas
+     * @return ar validus
+     */
+    private boolean isVectorInputValid(String input) {
+        return input.matches("[0-1]*"); // Turi būti nulis ar daugiau 1 arba 0
     }
 }
