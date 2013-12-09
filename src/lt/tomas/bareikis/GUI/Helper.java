622d8da88fe4eb5d81f80a5a68fdebedd30044b4
Tomas <tomuibar@gmail.com>
2013-12-10 00:04:42 +0200

    Comments and refactoring for Probability Text Field

--- src/lt/tomas/bareikis/GUI/Helper.java
+++ src/lt/tomas/bareikis/GUI/Helper.java
@@ -53,50 +53,6 @@ public class Helper {
         return input.matches("[0-1]*");
     }
 
-    /**
-     * Patikrina, ar įvesta tikimybė yra teisinga.
-     * Reguliari išraiška patikrina, ar įvesti skaičiai yra tokio formato:
-     *
-     * 0[kablelis arba taškas][bet kokio ilgio seka skaitemnų intervale 0 - 9]
-     * arba
-     * 0
-     * arba
-     * 1
-     * arba
-     * 1.0
-     *
-     * @param input tekstas
-     * @return ar įvestas tekstas atitinka tikimybės formatą
-     */
-    public static boolean isProbabilityValid(String input) {
-        return input.matches("(0[.,][0-9]+|0|1[.,]0|1)");
-    }
-
-    public static float readInputAsProbabilityFloat(String inputString) throws ParseException, IllegalArgumentException {
-        // Išmetame visus tarpus priekyje ir gale
-        inputString = inputString.trim();
-
-        if (!Helper.isProbabilityValid(inputString)) {
-            throw new IllegalArgumentException("Blogas tikimybės formatas. Tikimybė turi būti intervale [0, 1]");
-        }
-
-        // Pakeičiame kablelius taškais, kad išvengtume klaidų verčiant į slankaus kablelio skaičių
-        inputString = inputString.replace(",", ".");
-
-        DecimalFormat decimalFormat = new DecimalFormat();
-        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
-        // Slankaus kablelio skaičiaus sveikoji dalis nuo trupmeninės bus atskiriama tašku
-        decimalFormatSymbols.setDecimalSeparator('.');
-        decimalFormat.setDecimalFormatSymbols(decimalFormatSymbols);
-        Number parsedNumber = decimalFormat.parse(inputString);
-        float result = parsedNumber.floatValue();
-
-        if ((result <= 1.0f) && (result >= 0.0f)) {
-            return result;
-        } else {
-            throw new IllegalArgumentException("Blogas tikimybės formatas. Tikimybė turi būti intervale [0, 1]");
-        }
-    }
 
     public static String transformStringToBitString(String input) {
 
