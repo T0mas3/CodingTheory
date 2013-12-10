ff89697ca0e3be77643752d6d6c4ceb4d528b2dd
Tomas <tomuibar@gmail.com>
2013-12-10 21:38:01 +0200

    Added comments for Helper class

--- src/lt/tomasbareikis/GUI/Helper.java
+++ src/lt/tomasbareikis/GUI/Helper.java
@@ -6,23 +6,31 @@ import lt.tomasbareikis.DataStream;
 import lt.tomasbareikis.Decoder;
 import lt.tomasbareikis.Encoder;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+/**
+ * Pagalbinės funkcijos
+ */
 public class Helper {
 
+    /**
+     *
+     * Užkoduoja vektorių, paduotą kaip tekstinė eilutė
+     *
+     * @param vector vektorius tekstinės eilutės pavidalu
+     * @return užkoduotas vektorius tekstinės eilutės pavidalu
+     */
     public static String encodeVectorString(String vector) {
         Encoder encoder = new Encoder();
         DataStream encodedStream = encoder.encodeForSending(new DataStream(vector));
         return encodedStream.toStringOfBytes();
     }
 
+    /**
+     * Persiunčia vektorių kanalu ir jį iškraipo pagal klaidų tikimybę
+     *
+     * @param vector vektorius tekstinės eilutės pavidalu
+     * @param errorProbability klaidos tikimybė intervale [0, 1]
+     * @return vektorius, persiųstas kanalu
+     */
     public static String transferVectorString(String vector, float errorProbability) {
         Channel channel = new Channel(errorProbability);
 
@@ -31,6 +39,12 @@ public class Helper {
         return transferredStream.toStringOfBytes();
     }
 
+    /**
+     *  Atkoduoja vektorių
+     *
+     * @param vector vektorius tekstinės eilutės pavidalu
+     * @return atkoduotas vektorius tekstinės eilutės pavidalu
+     */
     public static String decodeVectorString(String vector) {
         DataStream transferredStream = new DataStream(vector);
         Decoder decoder = new Decoder();
@@ -39,87 +53,25 @@ public class Helper {
         return decodedStream.toStringOfBytes();
     }
 
+    /**
+     * Paverčia vektorių į simbolių eilutę
+     *
+     * @param input vektorius tekstinės eilutės pavidalu
+     * @return eilutė simbolių, užkoduotų duotame vektoriuje
+     */
     public static String transformBitStringToCharactersString(String input) {
 
         String charactersString = "";
-        int charBitLen = 16;
+        int charBitLen = 16; // Vienas simbolis koduojamas 16 bitų. Tam, kad būtų galima perduoti Lietuviškus simbolius
 
+        // Įvestą veiktorių skaidome į dalis po 16 bitų ir kiekvieną dalį verčiame į skaičių iš dvejetainės sistemos
         for (int i = 0; i < input.length() / charBitLen; i++) {
+            // Paverčiame dalį tekstinės eilutės į skaičių
             int characterInt = Integer.parseInt(input.substring(i*charBitLen, (i*charBitLen) + charBitLen), 2);
+            // Skaičių paverčiame simboliu (skaičius atitinka simbolio kodą)
             charactersString = charactersString + (char)characterInt;
         }
 
         return charactersString;
     }
-
-
-    public static String transformImageFileToBitString(File imageFile, String imageFormat) throws IllegalArgumentException, IOException {
-
-        imageFormat = imageFormat.toLowerCase();
-
-        if ((imageFormat.equals("jpg")) || (imageFormat.equals("png"))) {
-
-            byte[] imgBytes = Files.readAllBytes(Paths.get(imageFile.getPath()));
-
-            return getStringFromByteArray(imgBytes);
-        } else {
-            throw new IllegalArgumentException("Netinkamas failo formatas. Turi būti jpg arba png.");
-        }
-    }
-
-    public static byte[] transformBitStringToBitArray(String bytesString) throws IllegalArgumentException, IOException {
-
-        byte[] fileBytes = new byte[bytesString.length() / 8];
-
-        for (int i = 0; i < bytesString.length() / 8; i++) {
-            String byteString = bytesString.substring(i*8, (i*8)+8);
-            fileBytes[i] = (byte) Integer.parseInt(byteString, 2);
-        }
-
-        return fileBytes;
-    }
-
-    public static String transformImageToBitString(BufferedImage bufferedImage, String imageFormat) throws IllegalArgumentException, IOException {
-
-        imageFormat = imageFormat.toLowerCase();
-
-        if ((imageFormat.equals("jpg")) || (imageFormat.equals("png"))) {
-
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, imageFormat, byteArrayOutputStream);
-
-            int[] colors = new int[bufferedImage.getWidth() * bufferedImage.getHeight()];
-            bufferedImage.getRGB(0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(), colors, 0, bufferedImage.getWidth());
-
-            byte[] rgbImage = new byte[colors.length*3];
-
-            for (int i = 0; i < colors.length; i++) {
-                Color color = new Color(colors[i]);
-                rgbImage[i*3] = (byte)color.getRed();
-                rgbImage[i*3+1] = (byte)color.getGreen();
-                rgbImage[i*3+2] = (byte)color.getBlue();
-            }
-
-            return getStringFromByteArray(rgbImage);
-        } else {
-            throw new IllegalArgumentException("Netinkamas failo formatas. Turi būti jpg arba png.");
-        }
-    }
-
-    public static String getStringFromByteArray(byte[] bytes) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (byte imgByte: bytes) {
-            String singleByteString = Integer.toBinaryString(imgByte & 0xFF);
-
-            int addZeroes = 8 - singleByteString.length();
-            for (int n = 0; n < addZeroes; n++) {
-                singleByteString = "0" + singleByteString;
-            }
-            stringBuilder.append(singleByteString);
-        }
-        return stringBuilder.toString();
-    }
-
 }
